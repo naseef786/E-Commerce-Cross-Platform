27d7f41acf436259699dda0f32d282b25a8a729f
naseef786 <naseefac786@gmail.com>
2025-11-26 18:29:54 +0400

    updates

--- react-frontend/src/pages/Login.tsx
+++ react-frontend/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLoginMutation, setUser } from "../features/authSlice";
 import { useDispatch } from "react-redux";
